7fe9a0e630f57ac0ecb1115f5a8fe5450cf01ffe
aakashmaravind <aakashmaravind@gmail.com>
2025-09-04 21:30:39 +0530

    second commit

---
+++ src/main/java/com/samlportal/samlportal/SamlportalApplication.java
@@ -0,0 +1,15 @@
+package com.samlportal.samlportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@SpringBootApplication
+public class SamlportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlportalApplication.class, args);
+	}
+
+}
